24a7d57c78b27ed17fc1ca09f79d0bde0b36ebae
David-B-Comer <davec2233@gmail.com>
2021-02-22 10:12:45 -0500

    added app js

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -23,7 +23,7 @@ const UserProfiles = () => {
     return (
       <div key={index}>
         <h1>{userProfile.username}</h1>
-        <p>{userProfile.userProfileId}</p>
+        <p>{userProfile.userProfileID}</p>
       </div>
     );
   });
