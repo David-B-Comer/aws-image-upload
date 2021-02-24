ae88ddf57062c332f171f74b896fe0d2166f9b12
David-B-Comer <davec2233@gmail.com>
2021-02-24 12:39:55 -0500

    added upload user profile image type check

--- src/main/java/com/davec/awsimageupload/profile/UserProfileService.java
+++ src/main/java/com/davec/awsimageupload/profile/UserProfileService.java
@@ -1,11 +1,15 @@
 package com.davec.awsimageupload.profile;
 
+import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+
+import static org.apache.http.entity.ContentType.*;
 
 @Service
 public class UserProfileService {
@@ -28,6 +32,9 @@ public class UserProfileService {
             throw new IllegalStateException("Cannot upload empty file [ " + file.getSize() + "]");
         }
         // 2. If file is an image
+        if (!Arrays.asList(IMAGE_JPEG, IMAGE_PNG, IMAGE_GIF).contains(file.getContentType())) {
+            throw new IllegalStateException("File must be an image");
+        }
         // 3. The user exists in our database
         // 4. Grab metadata from file if any
         // 5. Store the image in s3 and update the database (userProfileImageLink) with s3 image link
