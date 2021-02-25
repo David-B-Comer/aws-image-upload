29a67a8bd94c541bfdcd20aca4886c71597d8daf
David-B-Comer <davec2233@gmail.com>
2021-02-25 09:38:23 -0500

    added user profile fixed uuid

--- src/main/java/com/davec/awsimageupload/datastore/FakeUserProfileDataStore.java
+++ src/main/java/com/davec/awsimageupload/datastore/FakeUserProfileDataStore.java
@@ -13,8 +13,8 @@ public class FakeUserProfileDataStore {
     private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
 
     static {
-        USER_PROFILES.add(new UserProfile(UUID.randomUUID(),"janetjones", null));
-        USER_PROFILES.add(new UserProfile(UUID.randomUUID(),"antoniojunior", null));
+        USER_PROFILES.add(new UserProfile(UUID.fromString("dc5620b4-8c5c-49ff-aa5e-360f41fa868f"),"janetjones", null));
+        USER_PROFILES.add(new UserProfile(UUID.fromString("9628c6e1-d1e9-41f1-8667-6197ca7c3c8d"),"antoniojunior", null));
     }
 
     public List<UserProfile> getUserProfiles() {
